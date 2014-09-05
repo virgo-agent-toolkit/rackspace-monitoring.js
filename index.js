a9511e993a6d4db99c6009e63b0679d3bd347139
Eugenia Gabrielova <eugenia.gabrielova@rackspace.com>
2014-09-04 18:14:46 -0700

    Refactors agent token retrieval and adds agent that connects to prod

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var request = require('request');
 var async = require('async');
 var virgo = require('virgo.js');
 var config = require('./config');
+var constants = require('./constants');
 var identity = require('./lib/identity');
 
 
@@ -10,35 +11,72 @@ var identityClient = identity.createClient(config.username, config.apiKey);
     agentServiceApiUrl = 'https://monitoring.api.rackspacecloud.com/v1.0/' + accountId + '/agent_tokens',
     agentLabel = 'virgo-js-agent';
 
-async.auto([
-  function getIdentityToken(callback) {
+function filterByVirgoAgentLabel(agentToken) {
+  return agentToken.label == agentLabel;
+}
+
+async.auto({
+  getIdentityToken: function getIdentityToken(callback) {
     identityClient.getToken(function(err, token) {
-      callback(err, token);
+      callback();
     });
   },
 
-  function createAgentTokenIfNoneExists(callback) {
-    function filterByVirgoAgentLabel(agentToken) {
-      return agentToken.label == agentLabel;
-    }
+  deleteVirgoTokens: ['getIdentityToken', function deleteExistingVirgoTokens(callback) {
+    // This will be useful for testing. Just good to get things working for now.
+    var tokensToDelete,
+        deletionUrl;
 
-    identityClient.sendAuthenticatedApiRequest(agentServiceApiUrl, "GET", {}, function(err, agentTokenList) {
-      var virgoJsAgents;
-      
-      if (agentTokenList.values) {
-        virgoJsAgents = agentTokenList.values.filter(filterByVirgoAgentLabel);
-      } else if (!virgoJsAgents) {
-        identityClient.sendAuthenticatedApiRequest(agentServiceApiUrl, "POST", {label: agentLabel}, callback);
-      } else {
+    identityClient.sendAuthenticatedApiRequest(agentServiceApiUrl, "GET", {}, function(err, agentTokens) {
+      tokensToDelete = agentTokens.values.filter(filterByVirgoAgentLabel);
+
+      async.each(tokensToDelete,
+        function(token, callback) {
+          deletionUrl = agentServiceApiUrl + '/' + token.id;
+
+          identityClient.sendAuthenticatedApiRequest(deletionUrl, "DELETE", {}, callback); 
+        },
+        function(err) {
+          callback();
+        }
+      );
+    });
+  }],
+
+  createVirgoToken: ['deleteVirgoTokens', function createVirgoAgentLabelToken(callback) {
+    identityClient.sendAuthenticatedApiRequest(agentServiceApiUrl, "POST", {label: agentLabel}, callback);
+  }],
+
+  getVirgoAgentToken: ['createVirgoToken', function getVirgoAgentLabelToken(callback) {
+    var virgoLabeledToken;
+
+    identityClient.sendAuthenticatedApiRequest(agentServiceApiUrl, "GET", {}, function(err, agentTokens) {
+      if (err) {
         callback(err);
+        return;
       }
+     
+      virgoLabeledToken = agentTokens.values.filter(filterByVirgoAgentLabel)[0];
+      callback(null, virgoLabeledToken);
     });
-  },
-], function (err) {
+  }],
+
+  deployVirgoAgent: ['getVirgoAgentToken', function createVirgoAgent(callback, results) {
+    var agent = virgo.agent;
+    var heartbeats = virgo.heartbeats;
+    
+    var agentOptions = {
+      host: constants.dfw,
+      token: results.getVirgoAgentToken.token
+    };
+    
+    agent([heartbeats()], agentOptions).run();
+    
+    callback();
+  }]
+}, function (err) {
   if (err) {
     console.log(err);
     return;
   }
-  console.log('Identity token obtained');
-  console.log('Agent token obtained');
 });
